511e79001ed9b32d06bdd9b8aeae05c2a3b61426
christopher.totanes <christopher.totanes@gmail.com>
2023-10-25 10:53:06 +0800

    Add level notice, game over screen

--- script.js
+++ script.js
@@ -23,8 +23,8 @@ let greenSet = {
     playGreen: function () {
         greenSelect.setAttribute ("class", "btn pressed");
         audioGreen.play();
-        setTimeout(() => greenSelect.removeAttribute ('class'), 500);
-        setTimeout(() => greenSelect.setAttribute("class", "btn green"), 500);
+        setTimeout(() => greenSelect.removeAttribute ('class'), 150);
+        setTimeout(() => greenSelect.setAttribute("class", "btn green"), 150);
         return green;
     }
 };
@@ -33,8 +33,8 @@ let redSet = {
     playRed: function () {
         redSelect.setAttribute ('class', 'btn pressed');
         audioRed.play();
-        setTimeout(() => redSelect.removeAttribute ('class'), 500);
-        setTimeout(() => redSelect.setAttribute("class", "btn red"), 500);
+        setTimeout(() => redSelect.removeAttribute ('class'), 150);
+        setTimeout(() => redSelect.setAttribute("class", "btn red"), 150);
         return red;
     }
 };
@@ -43,8 +43,8 @@ let yellowSet = {
     playYellow: function () {
         yellowSelect.setAttribute ('class', 'btn pressed');
         audioYellow.play();
-        setTimeout(() => yellowSelect.removeAttribute ('class'), 500);
-        setTimeout(() => yellowSelect.setAttribute("class", "btn yellow"), 500);
+        setTimeout(() => yellowSelect.removeAttribute ('class'), 150);
+        setTimeout(() => yellowSelect.setAttribute("class", "btn yellow"), 150);
         return yellow;
     }
 };
@@ -53,29 +53,38 @@ let blueSet = {
     playBlue: function () {
         blueSelect.setAttribute ('class', 'btn pressed');
         audioBlue.play();
-        setTimeout(() => blueSelect.removeAttribute ('class'), 500);
-        setTimeout(() => blueSelect.setAttribute("class", "btn blue"), 500);
+        setTimeout(() => blueSelect.removeAttribute ('class'), 150);
+        setTimeout(() => blueSelect.setAttribute("class", "btn blue"), 150);
         return blue;
     }
 };
 
-// Sleep setting ONLY works for async functions, user can input, while function waits
-const sleep = (delay) => new Promise((resolve) => setTimeout(resolve, delay * 1000))
+// Sleep setting ONLY works for async functions, user can input, while function waits. Takes seconds but will be multiplied with milliseconds for proper seconds output
+const sleep = (delaySeconds) => new Promise((resolve) => setTimeout(resolve, delaySeconds * 1000))
 
 // Starts Game
 let startGame = async () => {
     document.querySelector('.container').style.display = "block";
     document.getElementById('start').style.display = "none";
-    
-    for (let i = 0; i < 50; i++) {
+    document.getElementById('level-title').style.display = "none";
+    document.getElementById('level-lose').style.color = "#FEF2BF";
+
+    for (let i = 0; i < 20; i++) {
         console.log(i);
+
+        // shows level or iteration of sequence   
+        document.getElementById('level-lose').textContent = `Level ${i+1}`;
+
+        // will roll for random number + 1 then insert to an array
         chosenColor = Math.floor(Math.random() * 4) + 1;
         correctSequence.push(chosenColor);
         console.log(correctSequence);
+
+        // resets player's array where buttons pressed are stored
         playerAnswer.length = 0;
         console.log(playerAnswer)
 
-        await sleep(2);
+        await sleep(1);
         if (correctSequence[i] == green) {
             greenSet.playGreen();
         } else if (correctSequence[i] == red) {
@@ -115,8 +124,14 @@ let startGame = async () => {
         console.log(playerAnswer);
         console.log(correctSequence.toString());
         console.log(playerAnswer.toString())
+
+        // If statement will check both arrays in strings and see if will loop again or show player GameOver with level reached in i+1
         if (correctSequence.toString() != playerAnswer.toString()) {
-            console.log("Game Over");
+            document.querySelector('.container').style.display = "none";
+            document.getElementById('start').style.display = "block";
+            document.getElementById('level-title').style.display = "block";
+            document.getElementById('level-lose').style.color = "#f70505";
+            document.getElementById('level-lose').textContent = `Game Over! You reached Level ${i+1}!`;
             break;
         } else { () => {
             return playerAnswer.forEach(playerAnswer.pop())};
